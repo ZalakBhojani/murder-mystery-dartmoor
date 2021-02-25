7b1cf6fd8bee1f1636108eb81abe6a8c2f153a93
Zalak <zalakbhojani@yahoo.com>
2021-02-25 07:33:39 +0530

    improved readability

--- murder.pl
+++ murder.pl
@@ -1,6 +1,9 @@
-% To discover what happened to Mr. Zachery Carlisile	,younger brother of Alexa Carlisile, who was found dead inside a closed door room, you need to learn where each person was, what are the means, motives and opportunites of suicide or killing. Alexa had faked her death a day before, no one knew that she was awake until the morning of Zachery death
+% To discover what happened to Mr. Zachery Carlisile	,younger brother of Alexa Carlisile, who was found dead inside a closed door room, 
+% you need to learn where each person was, what are the means, motives and opportunites of suicide or killing. Alexa had faked her death a day before, 
+% no one knew that she was awake until the morning of Zachery death
 
-/* 1. To begin, we need to know the suspects. As per the information given by the buttler all the staff members and security were absent at 22.00, leaving behind 6 suspects. They are  
+/* 1. To begin, we need to know the suspects. As per the information given by the buttler all the staff members and security were absent at 22.00, 
+leaving behind 6 suspects. They are  
 	Gregory Carlisile - oldest child of Alexa
 	Edward Carlisile - middle child of Alexa
 	Rebecca Carlisile - youngest child of Alexa
@@ -60,25 +63,31 @@ writemmo(X,Y,Z):-
 
 
 % 2. Investigation of Zachery Room 
-% Clue 1: Door was locked from inside, a laptop, suicide letter and there were throat markings, which would mean a drug was recently taken. Recent history on the laptop indicate Zachery was shopping online not something expected from suicidal. A secret door leading to a hidden path, photo copy of floor plan and mystery switch from outside - would need a key to enter from outside
+% Clue 1: Door was locked from inside, a laptop, suicide letter and there were throat markings, which would mean a drug was recently taken. 
+% Recent history on the laptop indicate Zachery was shopping online not something expected from suicidal. A secret door leading to a hidden path, 
+% photo copy of floor plan and mystery switch from outside - would need a key to enter from outside
 
 mean(floor_plan). 
 
 
-% 3. Inspecting the suspects, we discover that Patrick (who was with Rosie, a maid), Gregory and Edward (they left the manor at half past eight) have alibi, rest do not. Rebecca found butler leave the room early that afternoon (the day of investigation) and seemed a bit startled when he saw Rebecca.
+% 3. Inspecting the suspects, we discover that Patrick (who was with Rosie, a maid), Gregory and Edward (they left the manor at half past eight) 
+% have alibi, rest do not. Rebecca found butler leave the room early that afternoon (the day of investigation) and seemed a bit startled when he saw Rebecca.
 
 alibi(patrick). alibi(gregory). alibi(edward). 
 
 
 % 4. We further go on to search the rooms of the suspects, starting with Rebecca.
-% Clue 2: Rebecca notes writing do not match with the suicide note and the laptop logs showcase that Rebecca was on call from 9 to midnight which verifies as her alibi. 
+% Clue 2: Rebecca notes writing do not match with the suicide note and the laptop logs showcase that Rebecca was on call from 9 to midnight 
+% which verifies as her alibi. 
 
 alibi(rebecca). 
 % ask mam regarding this - adding an option of suicide and https://stackoverflow.com/questions/15138706/cut-and-fail-in-prolog
 
 
 % 5. Next we investigate Mr. Fernsby office
-% Clue 3: Painkillers - enough to kill but not the poison used (enough to make Alexa believe) and Zachery's burnt diary where he confesses that he and Alexa murdered their elder brother 46 years ago and Fernsby made it look like an accident, the diary doesn't match the writing on the suicide note
+% Clue 3: Painkillers - enough to kill but not the poison used (enough to make Alexa believe) and Zachery's burnt diary where he 
+% confesses that he and Alexa murdered their elder brother 46 years ago and Fernsby made it look like an accident, the diary doesn't 
+% match the writing on the suicide note
 
 suicide(X) :- victim(X), !, fail.
 means(fernsby, pain_killers).
@@ -86,14 +95,17 @@ motive(fernsby, "Not Letting the confession go public").
 
 
 % 6. Next we investigate Emma and Gregory Room
-% % Clue 4: Letter from Emmas mother stating Emma is illegitimate child of Alexas brother Montgomery and claims to have witnessed Alexa and Zachary Murder him. Keychain pendant for the greenhouse and the key is missing. A Bulldog Cane - a sort of secret key (check out the library section) where we find out a letter that states Montgomery was ready to willingly let Alexa become the head of the family 
+% Clue 4: Letter from Emmas mother stating Emma is illegitimate child of Alexas brother Montgomery and claims to have witnessed Alexa 
+% and Zachary Murder him. Keychain pendant for the greenhouse and the key is missing. A Bulldog Cane - a sort of secret key (check out 
+% the library section) where we find out a letter that states Montgomery was ready to willingly let Alexa become the head of the family 
 
 motive(emma, "Revenge on the family").
 opportunity(emma, "With Alexa dead, and Zachery death by suicide would put her as the head of the family").
 
 
 % 7. It is worth investing Greenhouse as the keychain was found in the Emma and Gregory Room
-% Clue 5: Broken lab equipment, poison table showing lethal dosages for the poison to kill Zachery. Circle on the table: gender Female, age 65 - 76, 60 - 64 kg 
+% Clue 5: Broken lab equipment, poison table showing lethal dosages for the poison to kill Zachery. Circle on the table: gender Female, 
+% age 65 - 76, 60 - 64 kg 
 
 next_in_line(X):- 
 	X = alexa.
